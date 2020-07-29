c8da6a09867f27194fc3c9807c245dabcb50c992
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-29 10:52:50 +0530

    Update Multithreading.java

--- Multithreading.java
+++ Multithreading.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication2;
 
-/**
- *
- * @author admin
- */
-public class Multithreading {
-    
+package javaapplication2;
+class Test implements Runnable{
+    Thread t;
+    Test(){
+        t=new Thread(this);
+        t.start();
+    }
+    @Override
+    public void run(){
+        int i;
+        try{
+            for(i=0;i<5;i++){
+                System.out.println("Child Thread"+i);
+                Thread.sleep(500);
+            }
+        }catch(Exception e){
+            
+        }
+        
+    }
+}
+public class Multithreading
+{
+	public static void main(String[] args) {
+		Test t1=new Test();
+		int i;
+		try{
+		    for(i=0;i<5;i++){
+		        System.out.println("Main thread"+i);
+		        Thread.sleep(1000);
+		    }
+		}catch(Exception e){
+		    
+		}
+	}
 }
