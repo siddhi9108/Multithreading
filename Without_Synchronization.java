0b230edd92b5f4e346a042f011b7a4e203488fb8
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-29 10:35:44 +0530

    Update Without_Synchronization.java

--- Without_Synchronization.java
+++ Without_Synchronization.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication2;
 
-/**
- *
- * @author admin
- */
+class Share {
+
+    void show(String msg) {
+        try {
+            System.out.println("[" + msg);
+            Thread.sleep(500);
+            System.out.println("]");
+        } catch (Exception e) {
+
+        }
+    }
+}
+
+class asm implements Runnable {
+
+    Thread t;
+    String str;
+    Share ob;
+
+    asm(String n, Share ob) {
+        str = n;
+        ob = ob;
+        t = newThread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        ob.print(str);
+    }
+}
+
 public class Without_Synchronization {
-    
+
+    public static void main(String[] args) throws Exception {
+        Share ob = new Share();
+        asm t1 = new asm("A", ob);
+        asm t2 = new asm("B", ob);
+        asm t3 = new asm("C", ob);
+        try {
+            t1.t.join();
+            t2.t.join();
+            t3.t.join();
+
+        } catch (Exception e) {
+
+        }
+    }
 }
