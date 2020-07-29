d9f1611af86e4736478f48d48a93704feeded126
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-29 10:41:59 +0530

    Update Synchronized.java

--- Synchronized.java
+++ Synchronized.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication2;
 
-/**
- *
- * @author admin
- */
-public class Synchronized {
-    
+package javaapplication2;
+class Share{
+    synchronized void show(String msg){
+        try{
+            System.out.println("["+msg);
+            Thread.sleep(500);
+            System.out.println("]");
+        }catch(Exception e){
+            
+        }
+    }
+}
+class Test implements Runnable{
+    Thread t;
+    String str;
+    Share ob;
+    Test(String n, Share ob){
+        str=n;
+        ob=ob1;
+        t= newThread(this);
+        t.start();
+    }
+    @Override
+    public void run(){
+        ob.print(str);
+    }
+}
+public class Synchronized
+{
+	public static void main(String[] args) throws Exception {
+		Share ob=new Share();
+		Test t1=new Test("A",ob);
+		Test t2=new Test("B",ob);
+		Test t3=new Test("C",ob);
+		try{
+		    t1.t.join();
+		    t2.t.join();
+		    t3.t.join();
+		    
+		}catch(Exception e){
+		    
+		}
+	}
 }
